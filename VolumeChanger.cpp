d817f20c7c596fafd12edd8e2d8c0834c81cc741
Alex Hitech <ahitech@gmail.com>
2019-07-20 05:03:34 +0300

    Adding initial support for Mute.

--- VolumeChanger.cpp
+++ VolumeChanger.cpp
@@ -1,6 +1,7 @@
 #include "VolumeChanger.h"
 
 #include <Alert.h>
+#include <File.h>
 #include <MediaRoster.h>
 #include <String.h>
 
@@ -10,19 +11,111 @@
 
 VolumeChanger::VolumeChanger() 
 {	
-	previousVolumeLevel = MUTED;	// We start with non-muted sound
+	this->settings = new BMessage (SETTINGS_MESSAGE_CONSTANT);
+	GetSettings();
 }
 
 
 VolumeChanger::~VolumeChanger()
 {
-	if (previousVolumeLevel != MUTED)
-	{
-		UnMute();
-	}
+	SaveSettings();
 }
 
 
+/*
+ * This function fills the "settings" BMessage with settings of the program.
+ * It first attempts to read the settings from file. In case anything goes unsuccessfully,
+ * it fills the settings with defaults.
+ * If this file does not exist, or any error is encountered, a default settings set is created.
+ */
+status_t VolumeChanger::GetSettings ()
+{
+	status_t	toReturn = B_OK;
+	toReturn = ReadSettingsFromFile();		// Read info from the file
+				// Return value from ReadSettingsFromFile() is not checked.
+	toReturn = CheckSettings();				// Create a default settings set
+	
+	return (toReturn);		// Returning the result of reading the settings
+}
+
+
+/*
+ * This function tries to open the file with settings at the preset path.
+ * If successfully, it unflattens contents of the settings file into a BMessage.
+ * It is assumed that the BMessage for settings is allocated. 
+ *		It will be erased, but should be allocated!
+ */
+status_t VolumeChanger::ReadSettingsFromFile()
+{
+	status_t toReturn = B_OK;
+	BFile *readFrom = new BFile(PATH_TO_SETTINGS_FILE, B_READ_ONLY);
+	if ((toReturn = readFrom->InitCheck()) == B_OK)
+	{
+		toReturn = this->settings->Unflatten(readFrom);
+		readFrom->Unset();								// Close the file
+	}
+	delete readFrom;
+	return toReturn;
+}
+
+
+/*
+ * This function flattens contents of the settings message into the settings file
+ * at the predefined path.
+ */
+status_t VolumeChanger::SaveSettings ()
+{
+	status_t	toReturn = B_OK;
+	BFile *writeTo = new BFile(PATH_TO_SETTINGS_FILE, 	// Try opening the file
+							   B_WRITE_ONLY | B_CREATE_FILE | B_ERASE_FILE);
+	if ((toReturn = writeTo->InitCheck()) == B_OK)
+	{
+		toReturn = this->settings->Flatten (writeTo);	// Perform actual write
+		writeTo->Unset();								// Flush and close the file
+		delete writeTo;
+	}
+	return (toReturn);
+}
+
+
+/*
+ * After the settings are read, no matter if with error or not, they should be verified
+ * and any missing settings should be added.
+ * This function does exactly that.
+ * It is assumed that the "settings" BMessage is initialized.
+ */
+status_t	VolumeChanger::CheckSettings (void)
+{
+	status_t 	toReturn = B_OK;
+	type_code 	type = B_ANY_TYPE;
+	int32		countFound = 0;
+
+	// Sanity check
+	if (! settings)
+	{
+		return (B_NO_INIT);
+	}
+	
+	if (B_OK != settings->GetInfo(VOLUME_UP_KEY_NAME, &type, &countFound))
+	{
+		settings->AddInt32(VOLUME_UP_KEY_NAME, VOLUME_UP_KEY); 	
+	}
+	if (B_OK != settings->GetInfo(VOLUME_DOWN_KEY_NAME, &type, &countFound))
+	{
+		settings->AddInt32(VOLUME_DOWN_KEY_NAME, VOLUME_DOWN_KEY);
+	}
+	if (B_OK != settings->GetInfo(VOLUME_MUTE_KEY_NAME, &type, &countFound))
+	{
+		settings->AddInt32(VOLUME_MUTE_KEY_NAME, VOLUME_MUTE_KEY);
+	}
+	if (B_OK != settings->GetInfo(PREVIOUS_VOLUME_LEVEL_NAME, &type, &countFound))
+	{
+		settings->AddInt32(PREVIOUS_VOLUME_LEVEL_NAME, MUTED);
+	}
+	return B_OK;	
+}
+		
+		
 /*
  * This function receives ERROR or new volume level
  * 	If input is ERROR, current volume level is returned
@@ -119,28 +212,32 @@ status_t VolumeChanger::GetVolumeLevel (int* saveTo)
 }
 
 
-status_t VolumeChanger::Mute(void)
+int32 VolumeChanger::Mute(void)
 {
-	status_t status = GetVolumeLevel(&previousVolumeLevel);
+	int32 currentVolumeLevel;
+	status_t status = GetVolumeLevel(&currentVolumeLevel);
 	float dummy;
 	if (status == B_OK)
 	{
 		status = SetVolumeLevel (MUTED, &dummy);
 	}
-	return status;
+	if (status == B_OK)
+	{
+		settings->ReplaceInt32(PREVIOUS_VOLUME_LEVEL_NAME, currentVolumeLevel);
+		status = SaveSettings();		// Write changes to disk
+	}
+	return currentVolumeLevel;
 }
 
 
 status_t VolumeChanger::UnMute (void)
 {
-	if (previousVolumeLevel != MUTED)
-	{
-		int newLevel = previousVolumeLevel;
-		float dummy;
-		previousVolumeLevel = MUTED;
-		return SetVolumeLevel (newLevel, &dummy);
-	}
-	return B_OK;
+	int32 previousVolumeLevel;
+	float dummy;
+	settings->FindInt32(PREVIOUS_VOLUME_LEVEL_NAME, &previousVolumeLevel);
+	SetVolumeLevel (previousVolumeLevel, &dummy);
+	settings->ReplaceInt32(PREVIOUS_VOLUME_LEVEL_NAME, MUTED);
+	return SaveSettings();		// Write changes to disk
 }
 
 
@@ -152,34 +249,39 @@ filter_result VolumeChanger::Filter(BMessage* in,
 	int change = 0;
 	float dummy;
 	bool changeVolume = false;
+	
+	int32 volumeUpKey;
+	int32 volumeDownKey;
+	int32 volumeMuteKey;
+	int32 previousVolume;
+	
+	settings->FindInt32(VOLUME_UP_KEY_NAME, &volumeUpKey);
+	settings->FindInt32(VOLUME_DOWN_KEY_NAME, &volumeDownKey);
+	settings->FindInt32(VOLUME_MUTE_KEY_NAME, &volumeMuteKey);
+	settings->FindInt32(PREVIOUS_VOLUME_LEVEL_NAME, &previousVolume);
 
 	if (in->what == B_UNMAPPED_KEY_DOWN)
 	{
 
 		in->FindInt32 ("key", &key);
 		
-		switch (key)
+		if (key == volumeUpKey)
 		{
-			case VOLUME_UP_KEY:
-				change = +1;
-				break;
-			case VOLUME_DOWN_KEY:
-				change = -1;
-				break;
-			case VOLUME_MUTE_KEY:
-			 /*
-				if (previousVolumeLevel == MUTED)
-					Mute();
-				else
-					UnMute();
-			 */
-				// Intentional fall-through
-			 
-			default:
-				return B_DISPATCH_MESSAGE;
-				break;
-		};
-		
+			change = +1;
+		}
+		else if (key == volumeDownKey)
+		{
+			change = -1;
+		}
+		else if (key == volumeMuteKey)
+		{
+			if (previousVolume == MUTED) {
+				previousVolume = Mute();
+			} else {
+				UnMute();
+				previousVolume = MUTED;
+			}
+		}
 		if (change != 0)
 		{
 			if (GetVolumeLevel (&volume) != B_OK)
@@ -188,68 +290,12 @@ filter_result VolumeChanger::Filter(BMessage* in,
 			}
 			
 			SetVolumeLevel (volume + change, &dummy);
-//			return B_SKIP_MESSAGE;
 		}
 	}
 	
 	return B_DISPATCH_MESSAGE;
 }
 	
-
-
-/*
-
-filter_result VolumeChanger::Filter(BMessage* in,
-									BList* outlist)
-{
-	filter_result res = B_DISPATCH_MESSAGE;
-	if (in->what != B_UNMAPPED_KEY_DOWN)
-	{
-		return res;
-	}
-	
-	int32 key;
-	in->FindInt32 ("key", &key);
-	int volume;
-	int change;
-	float dummy;
-	switch (key)
-	{
-		case VOLUME_UP_KEY:
-			change = 1;
-			break;
-		case VOLUME_DOWN_KEY:
-			change = -1;
-			break;
-		case VOLUME_MUTE_KEY:
-			if (previousVolumeLevel == MUTED)
-				Mute();
-			else
-				UnMute();
-			
-			// Intentional fall-through
-		default:
-			return res;
-			break;
-	};
-	
-	if (previousVolumeLevel != MUTED)
-	{
-		SetVolumeLevel (previousVolumeLevel + change, &dummy);
-		previousVolumeLevel = MUTED;
-	} 
-	else 
-	{
-		if (GetVolumeLevel (&volume) != B_OK)
-			return res;
-		SetVolumeLevel (volume + change, &dummy);
-	}
-				
-	return res;
-}
-
-
-*/
 
 
 status_t VolumeChanger::InitCheck()
--- VolumeChanger.h
+++ VolumeChanger.h
@@ -26,9 +26,20 @@ extern "C" _EXPORT BInputServerFilter* instantiate_input_filter();
 #define		ERROR	MIN_INT
 	
 
+#define		SETTINGS_MESSAGE_CONSTANT	'sett'
+
 #define		VOLUME_UP_KEY		(0xC00E9)
 #define		VOLUME_DOWN_KEY		(0xC00EA)
 #define		VOLUME_MUTE_KEY		(0xC00E2)
+
+#define		PATH_TO_SETTINGS_FILE	\
+							"/boot/home/config/settings/VolumeChanger.txt"
+
+
+#define		VOLUME_UP_KEY_NAME			"Volume Up Key"
+#define		VOLUME_DOWN_KEY_NAME		"Volume Down Key"
+#define		VOLUME_MUTE_KEY_NAME		"Volume Mute Key"
+#define		PREVIOUS_VOLUME_LEVEL_NAME	"Previous Volume Level"
 
 
 
@@ -41,11 +52,16 @@ class VolumeChanger :
 		virtual status_t InitCheck();
 		virtual filter_result Filter(BMessage* message, BList* outlist);
 	
-	private:
-		int previousVolumeLevel;
+	private:		
+		// Settings section
+		BMessage* settings;
+		status_t ReadSettingsFromFile(void);
+		status_t GetSettings();
+		status_t SaveSettings();
+		status_t CheckSettings(void);
 		
 		FILE* logFile;
-		status_t Mute(void);
+		int32 Mute(void);
 		status_t UnMute (void);
 		status_t SetVolumeLevel(int In, float* Out);
 		status_t GetVolumeLevel(int *);
